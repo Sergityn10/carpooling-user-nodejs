c75f936b8e2bc05ac5e0dfbeb492b23057dcf98e
semartinl <sermarled10@gmail.com>
2026-01-25 07:53:47 +0100

    Mysql script to create payment intent db

--- app/schemas/paymentIntent.sql
+++ app/schemas/paymentIntent.sql
@@ -15,3 +15,25 @@ CREATE TABLE IF NOT EXISTS payment_intents (
     FOREIGN KEY (id_reserva) REFERENCES reservas(id_reserva)
     
 );
+-- MYSQL
+CREATE TABLE IF NOT EXISTS payment_intents (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    stripe_payment_id VARCHAR(255) DEFAULT NULL,
+    amount INT NOT NULL,
+    currency ENUM('eur', 'usd', 'gbp', 'jpy', 'aud') NOT NULL DEFAULT 'eur',
+    description TEXT DEFAULT NULL,
+    destination_account VARCHAR(255) DEFAULT NULL,
+    sender_account VARCHAR(255) DEFAULT NULL,
+    state VARCHAR(50) NOT NULL,
+    client_secret VARCHAR(255) DEFAULT NULL,
+    checkout_session_id VARCHAR(255) DEFAULT NULL,
+    id_reserva VARCHAR(255) DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_reserva 
+        FOREIGN KEY (id_reserva) 
+        REFERENCES reservas(id_reserva)
+        ON DELETE SET NULL 
+        ON UPDATE CASCADE
+) ENGINE=InnoDB;
